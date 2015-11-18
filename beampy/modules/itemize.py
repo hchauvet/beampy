01bebde3e81a9d721b8c2a8697baea486f75b8a7
odevauchelle <devauchelle@ipgp.fr>
2015-11-18 14:55:34 +0100

    Remove color_text function
    color_text now loaded from functions.

--- beampy/modules/itemize.py
+++ beampy/modules/itemize.py
@@ -8,22 +8,7 @@ Class to manage item lists for beampy
 from beampy import document
 from beampy.modules.text import text
 from beampy.modules.core import begingroup, endgroup
-from beampy.functions import convert_unit
-
-def color_text( textin, color ):
-	
-	'''
-		Adds Latex color to a string.
-	'''
-	
-	if "#" in    color:
-		textin = r'{\color[HTML]{%s} %s }'%( color.replace('#','').upper(), textin)
-	
-	else:
-		textin =r'{\color{%s} %s }'%( color, textin)
-
-	return textin
-
+from beampy.functions import convert_unit, color_text
 
 def itemize( items_list, x='center', y='auto', item_style = 'bullet', item_spacing = '+1cm' , item_indent = '0cm', item_color = 'default', text_color = 'default', width=None ):
 	
