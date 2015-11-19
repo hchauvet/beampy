23b52761f61141375f8fe62b62cba07a22972351
hchauvet <hugo.chauvet@gmail.com>
2015-11-19 09:40:41 +0100

    Add biblio module with cite function

---
+++ beampy/modules/biblio.py
@@ -0,0 +1,26 @@
+#-*- coding:utf-8 -*-
+from beampy import document
+from beampy.modules.text import text
+
+def cite( list_authors, x='center', y=580, color="default", size=10):
+    """
+    function to write citation on slide 
+    
+    Arguments
+    ---------
+    
+    list_authors : python list of authors
+    """
+    
+    citestr = '[' + ', '.join(list_authors) + ']'
+    
+    if color == 'default':
+        color = document._theme['title']['color']
+        
+    text( citestr, x=x, y=y, color=color, size=size)
+    
+   
+    
+ 
+    
+    
